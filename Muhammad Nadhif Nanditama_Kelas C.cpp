623288a2f5bcbc257bbebc7b3ecfe43355624add
nadhif21 <112993038+nadhif21@users.noreply.github.com>
2022-09-08 13:49:47 +0700

    Add files via upload

---
+++ Muhammad Nadhif Nanditama_Kelas C.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	cout <<"MASUKAN ANGKA \n" << "\n";
+	
+	int x;
+	cout<<"angka pertama :";
+	cin>>x;
+	
+	int y;
+	cout<<"angka kedua :";
+	cin>>y;
+	
+	cout <<"memiliki hasil :\n" << "\n";
+	
+	cout <<"jika ditambah adalah " << x + y << endl;
+	
+	cout <<"jika dikurangi adalah " << x - y << endl;
+	
+	cout <<"jika dikali adalah " << x * y << endl;
+	
+	cout <<"jika dibagi adalah " << x / y << endl;
+	
+	cout <<"dan sisa baginya adalah " << x % y << endl;
+	
+	return 0;
+	
+}
