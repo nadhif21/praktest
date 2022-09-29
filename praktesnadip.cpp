ca43d773e9a8e470adee30644ce1fe1d27757b10
nadhif21 <112993038+nadhif21@users.noreply.github.com>
2022-09-29 13:11:33 +0700

    Add files via upload

---
+++ praktesnadip.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int a;
+	cout<<"masukan angka pertama :";
+	cin>>a;
+	
+	int b;
+	cout<<"masukan angka kedua :";
+	cin>>b;
+	
+	for(
+		int c = a;c <= b;c += a){cout<<c<<" ";
+		}
+	
+	
+}
